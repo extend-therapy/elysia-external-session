2627ca008d8b300d241962532e52cbce278098ae
Eli Selkin <5606931+eselkin@users.noreply.github.com>
2025-05-29 22:28:57 -0700

    precommit

---
+++ src/Store/bunredis.d.ts
@@ -0,0 +1,26 @@
+import type { Duration } from "date-fns";
+import { BaseStore, type SessionOptions } from "./base";
+export interface BunRedisStoreOptions extends SessionOptions {
+    redisClient?: Bun.RedisClient;
+    redisOptions?: Bun.RedisOptions;
+    redisUrl?: string;
+    redisExpireAfter?: Duration;
+}
+/**
+ * WARNING: Does not work for Redis Clusters. Use RedisStore instead until Bun supports Redis Clusters.
+ */
+export declare class BunRedisStore<T> extends BaseStore<T> {
+    private redis;
+    private redisExpireAfterSeconds;
+    constructor(options: BunRedisStoreOptions);
+    get<T>({ sessionId }: {
+        sessionId: string;
+    }): Promise<T | null>;
+    set<T>({ sessionId, session }: {
+        sessionId: string;
+        session: T;
+    }): Promise<void>;
+    delete({ sessionId }: {
+        sessionId: string;
+    }): Promise<boolean>;
+}
